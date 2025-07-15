7e9d7d29a6aa6ff7ebfa3e741f2c28225f40ce11
Daehve <olansd04@gmail.com>
2025-07-15 23:08:59 +0100

    new feature

---
+++ myfirstcode.py
@@ -0,0 +1,14 @@
+#simple calculator
+##define your variable if necessary
+###float and input should be used to make your values decimal inclusive and to allow for a change in the values easily
+num_1=float(input("Enter your first value"))
+num_2=float(input("Enter your second value"))
+####define your operations
+sum_num=(num_1+num_2)
+product_num=(num_1*num_2)
+quotient_num=(num_1/num_2)
+subtract_num=(num_1-num_2)
+print(sum_num)
+print(product_num)
+print(quotient_num)
+print(subtract_num)
